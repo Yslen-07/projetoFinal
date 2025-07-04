a6e4e43fbf59095dc5e89dd46829e1986e8685f6
Yslen <lara.yslen61@aluno.ifce.edu.br>
2025-07-04 16:47:09 -0300

    Initial Commit

---
+++ projetoFinal/projetoFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  projetoFinalApp.swift
+//  projetoFinal
+//
+//  Created by Found on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct projetoFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
