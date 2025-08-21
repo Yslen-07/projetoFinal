cdcc18934b7e6aa62fc8be1b2728dbcd83acb7fb
OdilioCarneiro <odilionetocarneironogueira@gmail.com>
2025-08-21 16:34:12 -0300

    criacao do countDown

---
+++ projetoFinal/CountdownTimerView.swift
@@ -0,0 +1,18 @@
+//
+//  CountdownTimerView.swift
+//  projetoFinal
+//
+//  Created by found on 21/08/25.
+//
+
+import SwiftUI
+
+struct CountdownTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountdownTimerView()
+}
---
+++ projetoFinal/view/PecasView/CarouselEventoshj.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselEventoshj.swift
+//  projetoFinal
+//
+//  Created by found on 21/08/25.
+//
+
+import SwiftUI
+
+struct CarouselEventoshj: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselEventoshj()
+}
