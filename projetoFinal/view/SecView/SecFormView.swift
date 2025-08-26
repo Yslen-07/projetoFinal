93ed82f8eb91d4a420ed8a64f652bdf7a27adf93
Arlenson Lorran <arlensonlorran@gmail.com>
2025-08-26 17:23:19 -0300

    Novo formulario pra natacao e navegacao entre os dois formularios

--- projetoFinal/view/SecView/SecFormView.swift
+++ projetoFinal/view/SecView/SecFormView.swift
@@ -5,11 +5,12 @@ struct SecFormView: View {
     @Environment(\.modelContext) private var modelContext
     @Environment(\.dismiss) private var dismiss
     
+    @State private var formNatacao = false
     @State private var curso1: Curso = .informatica
     @State private var curso2: Curso = .edificacoes
     @State private var local: String = ""
     @State private var data: Date = Date()
-    @State private var categoriaSelecionada: CategoriaEsportiva = .natacao
+    @State private var categoriaSelecionada: CategoriaEsportiva = .volei
     @State private var genero: Genero = .mulher
     @State private var placar1: String = ""
     @State private var placar2: String = ""
@@ -21,6 +22,11 @@ struct SecFormView: View {
                     Picker("Categoria", selection: $categoriaSelecionada) {
                         ForEach(CategoriaEsportiva.allCases) { cat in
                             Text(cat.rawValue).tag(cat)
+                        }
+                    }
+                    .onChange(of: categoriaSelecionada) { oldValue, newValue in
+                        if newValue == .natacao {
+                            formNatacao = true
                         }
                     }
                     
@@ -44,14 +50,11 @@ struct SecFormView: View {
                         }
                     }
                 }
-                    
-                    Section("Placar") {
-                        TextField("Digite o placar de \(curso1.rawValue)", text: $placar1 )
-                        TextField("Digite o placar de \(curso2.rawValue)", text: $placar2 )
-                        
-                    }
-                    
                 
+                Section("Placar") {
+                    TextField("Digite o placar de \(curso1.rawValue)", text: $placar1)
+                    TextField("Digite o placar de \(curso2.rawValue)", text: $placar2)
+                }
                 
                 Section("Local e Data") {
                     TextField("Local", text: $local)
@@ -59,43 +62,45 @@ struct SecFormView: View {
                 }
                 
                 Section {
-                    VStack(spacing: 10) {
-                        Button("Salvar Jogo") {
-                            let novoJogo = Jogo(
-                                curso1: curso1,
-                                curso2: curso2,
-                                categoria: categoriaSelecionada,
-                                genero: genero,
-                                local: local,
-                                data: data,
-                                placar1: placar1,
-                                placar2: placar2
-                            )
-                            modelContext.insert(novoJogo)
-                            dismiss()
-                        }
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+                    Button("Salvar Jogo") {
+                        let novoJogo = Jogo(
+                            curso1: curso1,
+                            curso2: curso2,
+                            categoria: categoriaSelecionada,
+                            genero: genero,
+                            local: local,
+                            data: data,
+                            placar1: placar1,
+                            placar2: placar2
+                        )
+                        modelContext.insert(novoJogo)
+                        dismiss()
                     }
-                    Section{
-                        Button("Cancelar") {
-                            dismiss()
-                        }
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.gray.opacity(0.3))
-                        .foregroundColor(.black)
-                        .cornerRadius(10)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    
+                    Button("Cancelar") {
+                        dismiss()
                     }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.gray.opacity(0.3))
+                    .foregroundColor(.black)
+                    .cornerRadius(10)
                 }
+                
             }
             .navigationTitle("Novo Jogo")
+            .navigationDestination(isPresented: $formNatacao) {
+                SecNatacaoView()
+            }
         }
     }
 }
+
 #Preview{
     SecFormView()
             .modelContainer(for: Jogo.self, inMemory: true)
